e84b7567589795a55cc705cb47478c3176272374
Joshua Matthews <josh@jmatth.com>
2022-09-06 13:54:06 -0400

    Connecting dog browser to dog.ceo API

--- src/ts/apps/dogBrowser.ts
+++ src/ts/apps/dogBrowser.ts
@@ -1,6 +1,8 @@
 import { moduleName } from "../constants";
 
 export default class DogBrowser extends Application {
+  private imageUrl? = "";
+
   override get title(): string {
     return "Dog Browser";
   }
@@ -10,7 +12,45 @@ export default class DogBrowser extends Application {
       id: "dog-browser",
       template: `modules/${moduleName}/templates/dogs.hbs`,
       width: 720,
-      height: "auto",
+      height: 720,
     }) as ApplicationOptions;
+  }
+
+  override getData() {
+    return {
+      imageUrl: this.imageUrl,
+    };
+  }
+
+  override activateListeners(html: JQuery<HTMLElement>): void {
+    super.activateListeners(html);
+    html
+      .find("button.module-control")
+      .on("click", this._onClickControlButton.bind(this));
+  }
+
+  async _onClickControlButton(event: Event): Promise<void> {
+    event.preventDefault();
+    const button = event.currentTarget as HTMLElement;
+    const action = button.dataset.action;
+    // const module = (game as Game).modules.get(moduleName) as MyModule;
+
+    switch (action) {
+      case "randomize-dog":
+        this._randomizeDog();
+        break;
+    }
+  }
+
+  async _randomizeDog() {
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
+    if (response.status != 200) {
+      ui.notifications?.error(
+        `Unexpected response fetching new dog image: ${response.status}: ${response.statusText}`
+      );
+      return;
+    }
+    this.imageUrl = (await response.json()).message;
+    this.render();
   }
 }
